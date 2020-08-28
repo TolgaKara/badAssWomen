b8949cdfa98462da1d2e607414f9d8ac3a1ee5c6
Anam Zehra <anamzehra@gmail.com>
2020-08-28 11:18:14 +0200

    fix points error and add try again button

--- start.js
+++ start.js
@@ -2,7 +2,8 @@ function submitForm(e) {
     e.preventDefault();
     let name = document.forms["welcome_form"]["name"].value;
   
+    points = 0;
     sessionStorage.setItem("name", name);
-  
+    sessionStorage.setItem("points", points);
     location.href = "quiz.html";
   }
